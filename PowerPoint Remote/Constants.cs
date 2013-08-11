187782da885a007480e792a6ba534cdcbe4e785e
Sören Gade <gade.soeren@googlemail.com>
2013-08-11 17:56:28 +0200

    Increased announce interval.

--- PowerPoint Remote/Constants.cs
+++ PowerPoint Remote/Constants.cs
@@ -12,7 +12,7 @@ namespace PowerPoint_Remote
         public const int SERVER_PAIRINGCODELENGTH = 6;
 
         public const String SERVER_IPRANGE = "255.255.255.255";
-        public const int SERVER_ANNOUNCEINTERVAL = 1000;
+        public const int SERVER_ANNOUNCEINTERVAL = 3000;
         public const String SERVER_ANNOUNCESTRING = Constants.NAME + "-{0}"; // 0 == presentation name
     }
 }
