1c057bd62cebb897a15f66d98fc9f2ad7d274183
Sören Gade <gade.soeren@googlemail.com>
2013-08-17 23:13:39 +0200

    Added tons of comments to Constants.

--- PowerPoint Remote/Constants.cs
+++ PowerPoint Remote/Constants.cs
@@ -3,20 +3,54 @@ using System.Text;
 
 namespace PowerPoint_Remote
 {
+    /// <summary>
+    /// Constants for runtime.
+    /// </summary>
     public abstract class Constants
     {
+        /// <summary>
+        /// Name of the application.
+        /// </summary>
         public const String NAME = "PowerPoint Remote";
+        /// <summary>
+        /// Defualt encoding used in this application.
+        /// </summary>
         public static readonly Encoding ENCODING = Encoding.UTF8;
 
+        /// <summary>
+        /// The port the server is listening on for clients.
+        /// </summary>
         public const int SERVER_PORT = 34012;
+        /// <summary>
+        /// The length of the pairing code to be generated.
+        /// </summary>
         public const int SERVER_PAIRINGCODELENGTH = 6;
 
+        /// <summary>
+        /// Filetype of the slide export to be used.
+        /// </summary>
         public const String EXPORT_FILETYPE = "PNG";
+        /// <summary>
+        /// Width of the slide to be exported.
+        /// </summary>
         public const int EXPORT_WIDTH = 1024;
+        /// <summary>
+        /// Height of the slide to be exported.
+        /// </summary>
         public const int EXPORT_HEIGHT = 768;
 
+        /// <summary>
+        /// The IP range the server is announcing itself on.
+        /// 255.255.255.255 is everywhere.
+        /// </summary>
         public const String SERVER_IPRANGE = "255.255.255.255";
+        /// <summary>
+        /// Interval (in ms) the server is announcing itself when no client is connected.
+        /// </summary>
         public const int SERVER_ANNOUNCEINTERVAL = 3000;
+        /// <summary>
+        /// The format string the server should fill when announcing itself.
+        /// </summary>
         public const String SERVER_ANNOUNCESTRING = Constants.NAME + "-{0}"; // 0 == presentation name
     }
 }
