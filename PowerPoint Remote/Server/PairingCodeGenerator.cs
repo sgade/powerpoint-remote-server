3ea77da4ecd95cc76390fa8ee1796f0c512663c9
Sören Gade <gade.soeren@googlemail.com>
2013-08-17 23:08:15 +0200

    Added comments to PairingCodeGenerator.

--- PowerPoint Remote/Server/PairingCodeGenerator.cs
+++ PowerPoint Remote/Server/PairingCodeGenerator.cs
@@ -2,10 +2,13 @@
 
 namespace PowerPoint_Remote.Server
 {
+    /// <summary>
+    /// A helper class that can generate some nice random codes for us.
+    /// </summary>
     public abstract class PairingCodeGenerator
     {
         /// <summary>
-        /// All the chars that are shown
+        /// All the chars that are used in the algorithm.
         /// </summary>
         private static readonly char[] chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                                                  /* 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',  */
